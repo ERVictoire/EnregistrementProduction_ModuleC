0ba5cd6e8a21b5410930a520bdcb66bb049a01d8
charles12-kapend <charlekapend875@gmail.com>
2026-02-09 16:58:20 +0200

    Ajouts des commentaires dans les lignes des codes

--- src/main/java/net/ista2025/bac2IFI/production/ProductionData.java
+++ src/main/java/net/ista2025/bac2IFI/production/ProductionData.java
@@ -1,13 +1,25 @@
 package net.ista2025.bac2IFI.production;
 
+// classe modèle représentant une saisie de production.
+// contient les données brutes saisie par l'utilisateur.
 public class ProductionData {
 
+  //Data de production (format texte YYY-MM-DD)
     public String date;
+
+// Identifiant de l'opérateur ayant efffectué la production
     public String operatorId;
+
+   // Nom ou ou referencce de la pièce produite
     public String piece;
+
+   //Quantité des piéces conformes
     public int qtyOK;
+
+   // Quantité des pièces non conformes
     public int qtyKO;
 
+    // Constructeur permettant d'initialiser toutes les données de production
     public ProductionData(String date, String operatorId, String piece, int qtyOK, int qtyKO){
         this.date = date;
         this.operatorId = operatorId;
--- src/main/java/net/ista2025/bac2IFI/production/ProductionInput.java
+++ src/main/java/net/ista2025/bac2IFI/production/ProductionInput.java
@@ -1,26 +1,32 @@
 package net.ista2025.bac2IFI.production;
 
-
 import java.util.Scanner;
 
+// Classe responsable de la saisie des données utilisateur depuis la console.
 public class ProductionInput {
 
     public  ProductionData saisirProduction () {
 
+        // Scanner utilisé pour lire les entrées clavier
         Scanner scanner = new Scanner(System.in);
 
+        // Demande et récupère la date de production
         System.out.println("Date (AAAA-MM-JJ) : ");
         String date = scanner.nextLine();
 
+        // Demande et récupère l'identifiant de l'opérateur
         System.out.println("Operator ID : ");
         String operatorId = scanner.nextLine();
 
+        // Demande et récupère la pièce produite
         System.out.println("Piece :");
         String piece = scanner.nextLine();
 
+        // Demande et récupère la quantité OK
         System.out.println("Quantité ok : ");
         int qtyOK = Integer.parseInt(scanner.nextLine());
 
+        // Demande et récupère la quantité KO
         System.out.println("Quantinté KO ");
         int qtyKO = Integer.parseInt(scanner.nextLine());
 
